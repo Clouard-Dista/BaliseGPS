a1d48d2c89c820251eb9b7a6bdf97b24066a3a53
Talloir Vincent <vincenttall@hotmail.fr>
2019-05-20 22:12:56 +0200

    separation of features GPS

--- gps/gps.ino
+++ gps/gps.ino
@@ -1,11 +1,6 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 
- 
-#define SIM800_RX_PIN D5
-#define SIM800_TX_PIN D6
-SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
-
 #define GPS_RX_PIN D3
 #define GPS_TX_PIN D4
 long lat,lon; // create variable for latitude and longitude object
@@ -19,18 +14,6 @@ void setup(){
   gpsSerial.begin(9600); // connect gps sensor
   pinMode(GPS_RX_PIN, INPUT);
   pinMode(GPS_TX_PIN, OUTPUT);
-  Serial.println("Setup Complete!");
-  
-  Serial.println("Sending SMS...");
-  serialSIM800.write("AT+CMGF=1\r\n");
-  delay(1000);
-  serialSIM800.write("AT+CMGS=\"0608359137\"\r\n");
-  delay(1000);
-  serialSIM800.write("TEST");
-  delay(1000);
-  serialSIM800.write((char)26);
-  delay(1000);
-  Serial.println("SMS Sent!");
 }
 
 void loop(){
@@ -39,7 +22,7 @@ void loop(){
     gps.get_position(&lat,&lon); // get latitude and longitude
     Serial.print("lat: ");
     Serial.print(lat);
-    Serial.print("          ");
+    Serial.print("//");
     Serial.print("lon: ");
     Serial.println(lon);
    }
